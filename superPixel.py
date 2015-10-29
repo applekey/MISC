a118e1bfa123a72834276f8481caefa1d3f51bf5
vincent <applekey@cs.uoregon.edu>
2015-10-29 15:24:25 -0700

    fdsa

--- superPixel.py
+++ superPixel.py
@@ -74,11 +74,23 @@ def TraceSpecular(edges):
         #create the ray
         sampleRay = ray(camPos,vecNorm(direction))
 
-        xIndex = start + (end-start)/samples * i + random.uniform(0,0.3)
+        xIndex = start + (end-start)/samples * i #+ random.uniform(0,0.3)
         edge = findCorrectEdgeRay(edges,sampleRay)
 
         if edge == None:
             continue
+
+        #find rayslope
+        rayDir = ray.direction[1]/ray.direction[0]
+        raym = ray.position[1] - rayDir* ray.position[0]
+        #find segmentslope
+        segSlope = (edge[1][1] - edge[0][1])/(edge[1][0] - edge[0][0])
+        segm = edge[1][1] - rayDir* edge[1][0]
+
+
+        
+
+
         pYpos =  edge[0][1] + (-edge[0][0] + xIndex)/(edge[1][0] - edge[0][0]) * (edge[1][1] - edge[0][1])
         slope = [(edge[0][1] - edge[1][1]),(edge[0][0] - edge[1][0])]
         normal = slope
@@ -95,10 +107,9 @@ def TraceDiffuse(edges):
     lightPos = [10,15]
 
     for i in range(samples):
-        xIndex = start + (end-start)/samples * i + random.uniform(0,0.3)
+        xIndex = start + (end-start)/samples * i #+ random.uniform(0,0.3)
         edge = findCorrectEdge(edges,xIndex)
         ## calculate lambert
-            #interpolate y position
         if edge == None:
             continue
         pYpos =  edge[0][1] + (-edge[0][0] + xIndex)/(edge[1][0] - edge[0][0]) * (edge[1][1] - edge[0][1])
@@ -133,7 +144,7 @@ def output(colors):
     y = [blank_image[0][i] for i in range(lenColors)]
     plt.scatter(x, y)
     plt.show()
-    cv2.imwrite('/Users/applekey/Desktop/c.png',blank_image)
+    cv2.imwrite('/Users/vincent/Desktop/c.png',blank_image)
 
     # cv2.imshow('image',blank_image)
     # cv2.waitKey(0)
@@ -147,7 +158,7 @@ def main():
     plen = len(points)
     print plen
 
-    skip = 6
+    skip = 1
     for s in range(skip):
         for i in range(plen)[10+s::skip]:
             edges.append([points[i-skip],points[i]])
@@ -156,7 +167,7 @@ def main():
         # for edge in edges:
         #     print str(edge[0][0]) + ' ' + str(edge[1][0])
 
-        colors = TraceDiffuse(edges)
+        colors = TraceSpecular(edges)
         allColors.append(colors)
         edges = []
     output(allColors)
